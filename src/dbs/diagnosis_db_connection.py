1825fe9a45c7b85b93581190355f91ceceabf270
toakes-sim <tom.oakes@hotmail.co.uk>
2026-06-06 12:55:37 +0100

    added connections to structured data storage using mysql

---
+++ src/dbs/diagnosis_db_connection.py
@@ -0,0 +1,57 @@
+import mysql.connector
+import os
+
+class DiagnosisDBConnection():
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            user=os.environ['MYSQLUSERNAME'],
+            password=os.environ['MYSQLPASSWORD'],
+            host='localhost',
+            database='national_health_monitoring')
+        self.cursor = self.cnx.cursor(dictionary=True)
+        # Try and create the relevant table if it does not exist
+        try:
+            query = ("CREATE TABLE IF NOT EXISTS diagnosisdb ("
+                     "appointment_id VARCHAR(255) PRIMARY KEY, "
+                     "actual_patient_symptom_list VARCHAR(255), "
+                     "actual_patient_infection_status VARCHAR(255), "
+                     "inferred_patient_symptom_list VARCHAR(255), "
+                     "inferred_patient_infection_status VARCHAR(255), "
+                     "diagnosis_summary VARCHAR(510))")
+            # set the diagnosis db up
+            self.cursor.execute(query)
+
+            self.cnx.commit()
+
+        except mysql.connector.Error as err:
+            print(err)
+
+    def add_record_of_diagnosis(self, diagnosis_details):
+        query = ("INSERT INTO diagnosisdb ("
+                 "appointment_id, "
+                 "actual_patient_symptom_list, "
+                 "actual_patient_infection_status, "
+                 "inferred_patient_symptom_list, "
+                 "inferred_patient_infection_status, "
+                 "diagnosis_summary) "
+                 "VALUES (%s, %s, %s, %s, %s, %s)")
+        diagnosis_data = (diagnosis_details['appointment_id'],
+                          str(diagnosis_details['actual_patient_symptom_list']),
+                          diagnosis_details['actual_patient_infection_status'],
+                          str(diagnosis_details['inferred_patient_symptom_list']),
+                          diagnosis_details['inferred_patient_infection_status'],
+                          diagnosis_details['diagnosis_summary'])
+        self.cursor.execute(query, diagnosis_data)
+        self.cnx.commit()
+
+    def check_if_diagnosis_exist(self, appointment_id):
+        query = "SELECT * FROM diagnosisdb WHERE appointment_id = %s"
+        self.cursor.execute(query, (appointment_id,))
+        results = self.cursor.fetchall()
+        if len(results) > 0:
+            return 'exist'
+        else:
+            return 'deos not exist'
+
+    def close(self):
+        self.cnx.close()
---
+++ src/dbs/patient_db_connection.py
@@ -0,0 +1,61 @@
+import mysql.connector
+import uuid
+import os
+
+class PatientDBConnection():
+    def __init__(self):
+        self.cnx = mysql.connector.connect(
+            user=os.environ['MYSQLUSERNAME'],
+            password=os.environ['MYSQLPASSWORD'],
+            host='localhost',
+            database='national_health_monitoring')
+        self.cursor = self.cnx.cursor(dictionary=True)
+        # Try and create the relevant table if it does not exist
+        try:
+            query = ("CREATE TABLE IF NOT EXISTS patientdb ("
+                     "patient_id VARCHAR(255) PRIMARY KEY, "
+                     "surgery_location VARCHAR(255), "
+                     "doctor_id VARCHAR(255), "
+                     "patient_age INT, "
+                     "patient_persona VARCHAR(510), "
+                     "infection_status VARCHAR(255))")
+            # set the patients db up
+            self.cursor.execute(query)
+            self.cnx.commit()
+
+        except mysql.connector.Error as err:
+            print(err)
+
+    def add_record_of_patient(self, surgery_location, doctor_id, patient_age, patient_persona, infection_status):
+        patient_id = self.generate_uuid()
+        query = ("INSERT INTO patientdb ("
+                 "patient_id, "
+                 "surgery_location, "
+                 "doctor_id, "
+                 "patient_age, "
+                 "patient_persona, "
+                 "infection_status) "
+                 "VALUES (%s, %s, %s, %s, %s, %s)")
+        patient_data = (
+            patient_id,
+            surgery_location,
+            doctor_id,
+            patient_age,
+            patient_persona,
+            infection_status)
+        self.cursor.execute(query, patient_data)
+        self.cnx.commit()
+        return patient_id
+
+    def get_record_of_patient(self, patient_id):
+        query = "SELECT * FROM patientdb WHERE patient_id = %s"
+        self.cursor.execute(query, (patient_id,))
+        results = self.cursor.fetchall()
+        return results
+
+    @staticmethod
+    def generate_uuid():
+        return str(uuid.uuid4())
+
+    def close(self):
+        self.cnx.close()
